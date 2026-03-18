d5cdc8b7e4b602cffebdff649323f23941a04dda
Joshua Nzuzi <nzuzijoshua@gmail.com>
2026-03-18 03:03:05 +0100

    Set Public URL Networking for Production

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/foundation.dart' show kIsWeb;
 
 class ApiConfig {
-  // REMPLACEZ CETTE URL PAR VOTRE VRAIE URL RAILWAY
-  static const String _productionUrl = "https://votre-backend.up.railway.app"; 
+  // URL officielle de production sur Railway
+  static const String _productionUrl = "https://nexa-backend-production-82d7.up.railway.app"; 
 
   static String get baseUrl {
     if (kIsWeb) {
-      return "http://localhost:3000"; // Pour le test web local
+      // Pour le test web local ou mobile via IP locale (si besoin de repasser en local)
+      // return "http://10.221.227.226:3000"; 
+      return "http://localhost:3000"; 
     } else {
-      // Pour le mobile, on utilise désormais l'URL du Cloud
+      // Pour le mobile, on utilise désormais l'URL du Cloud Railway
       return _productionUrl;
     }
   }
